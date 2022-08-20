999558ea7ae0462032c65bd3f14bf1f1055f0dfc
Keatliel <thelastor3o@gmail.com>
2022-08-20 03:22:14 -0600

    Fixed spelling error causing fatal crashes

--- src/main/java/io/github/keatliel/fabulis_antiquis/FabulisAntiquis.java
+++ src/main/java/io/github/keatliel/fabulis_antiquis/FabulisAntiquis.java
@@ -1,15 +1,15 @@
-package com.example.example_mod;
+package io.github.keatliel.fabulis_antiquis;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class FabulisAntiquis implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("Fabulis Antiquis");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
--- src/main/java/io/github/keatliel/fabulis_antiquis/mixin/TitleScreenMixin.java
+++ src/main/java/io/github/keatliel/fabulis_antiquis/mixin/TitleScreenMixin.java
@@ -1,16 +1,17 @@
-package com.example.example_mod.mixin;
+package io.github.keatliel.fabulis_antiquis.mixin;
 
-import com.example.example_mod.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import io.github.keatliel.fabulis_antiquis.FabulisAntiquis;
+
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		FabulisAntiquis.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
